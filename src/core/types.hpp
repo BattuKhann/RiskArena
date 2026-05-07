e43fdae4e0ff79c1669cb9672535d79abfd898e6
Yusef Hosny <hosny.yusef@gmail.com>
2026-05-06 21:41:05 -0400

    Fixed Issues, Added Card Deck

--- src/core/types.hpp
+++ src/core/types.hpp
@@ -4,7 +4,36 @@
 #define MAX_TERRITORIES 64
 #define MAX_CONTINENTS 64
 
-const struct MapData{
+enum ActionType : uint8_t{
+    END_PHASE,
+    TRADE,
+    STANDARD
+};
+
+enum GamePhase : uint8_t{
+    MOVE,
+    SETUP,
+    DRAFT,
+    ATTACK,
+    FORTIFY
+};
+
+struct Action{
+    ActionType type;
+    uint64_t SourceID; //Doubles as # of Infantry Cards
+    uint64_t TargetID; //Doubles as # of Cavalry Cards
+    uint64_t Amount; //Doubles as # of Artillery Cards
+};
+
+struct Result{
+    bool moveRequired;
+    bool territoryTaken;
+    int aLosses;
+    int dLosses;
+    int playerEliminated;
+};
+
+struct MapData{
     uint64_t adjacency[MAX_TERRITORIES] = {0};
     uint64_t continents[MAX_CONTINENTS] = {0};
     int bonuses[MAX_CONTINENTS] = {0};
@@ -20,48 +49,25 @@ struct GameData{
     bool capitals;
 };
 
+struct Card {
+    uint64_t territoryID;
+    uint8_t type;
+};
+
 struct MapState{
-    // Current Game Flow
+    uint64_t ownedMasks[MAX_PLAYERS];
+    uint64_t playerCards[MAX_PLAYERS];
+    uint64_t drawDeckMask; //Available cards to draw
+
+    uint16_t territoryTroops[MAX_TERRITORIES];
+    uint16_t troopsToPlace;
+
+    uint8_t aliveFlags = 0x3F;
     uint8_t currPlayer;
     GamePhase phase = GamePhase::SETUP;
     bool hasConqueredThisTurn;
     int globalSetBonus;
+    int turn;
 
-    //Player Data
-    int aliveFlags = 0x3F;
-    int troopsToPlace[MAX_PLAYERS] = {0};
-    uint64_t ownedMasks[MAX_PLAYERS] = {0};
-    int playerCards[MAX_PLAYERS] = {0};
-
-    //Territory Data
-    uint16_t territoryState[MAX_TERRITORIES] = {0};
-};
-
-struct Action{
-    ActionType type;
-    int SourceID;
-    int TargetID;
-    int Amount;
-};
-
-struct Result{
-    bool moveRequired;
-    bool territoryTaken;
-    int aLosses;
-    int dLosses;
-    int playerEliminated;
-};
-
-enum class GamePhase{
-    MOVE,
-    SETUP,
-    DRAFT,
-    ATTACK,
-    FORTIFY
-};
-
-enum class ActionType{
-    END_PHASE,
-    TRADE,
-    STANDARD
+    Card gameDeck[MAX_TERRITORIES];
 };
